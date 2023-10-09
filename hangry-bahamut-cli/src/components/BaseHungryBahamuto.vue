8d8d1db95669de13e228b7d6f3e7cc708015a858
watao-o <thbc.watao@gmail.com>
2023-10-09 17:27:17 +0900

    add:install vuetify

---
+++ hangry-bahamut-cli/src/components/BaseHungryBahamuto.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-container class="hello">
+    <v-row>
+      <v-col
+        v-for="n in 12"
+        :key="n"
+        cols="2"
+      >
+        はらぺこバハムート{{ n }}
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'BaseHungryBahamuto',
+  props: {
+  }
+}
+</script>
+
+<style scoped>
+</style>
---
+++ hangry-bahamut-cli/src/plugins/vuetify.js
@@ -0,0 +1,12 @@
+// Styles
+import 'vuetify/styles'
+
+// Vuetify
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+export default createVuetify({
+  components,
+  directives
+})
